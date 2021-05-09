6f3bedd86cda8cb0e98c5e0636edac9cac80ccb2
Brian Musembi <brian.musembi@mths.ca>
2021-05-09 21:20:09 +0000

    added grandmother_approval.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created by Brian Musembi
+# Created on May 2021
+# This module contains constants
+
+MIN_AGE = 25
+MAX_AGE = 40
---
+++ grandmother_approval.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Created by Brian Musembi
+# Created on May 2021
+# This program determines if the grandmother will approve of the user
+#     dating her grandchild
+
+import constants
+
+
+def main():
+    # this function will determine if the user is in the needed age range
+
+    print("This program determines if you can date Grandmother's grandchild")
+    print("")
+
+    # input
+    user_age = input("Input your age as an integer: ")
+    print("")
+
+    # process
+    try:
+        user_age_int = int(user_age)
+
+        if user_age_int > constants.MIN_AGE and \
+           user_age_int < constants.MAX_AGE:
+            # output
+            print("Amazing! Grandmother approves of you"
+                  " dating her grandchild.")
+
+        elif user_age_int < constants.MIN_AGE:
+            # output
+            print("Sorry, you are too young!.")
+
+        elif user_age_int > constants.MAX_AGE:
+            # output
+            print("Sorry, you are too old!")
+
+    except Exception:
+        # output
+        print("That's not a number! Try again.")
+
+    finally:
+        # output
+        print("")
+        print("Better luck with someone else!")
+
+
+if __name__ == "__main__":
+    main()
